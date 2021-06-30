6e039f5c297e65eac2fa610e757dca96e8f134f5
PavanDongare <dongare.pavan25@gmail.com>
2021-07-01 00:51:00 +0530

    wip

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,13 +5,13 @@ import Image from 'next/image'
 function Header() {
     return (
 
-        <div className="bg-gray-800 p-5 shadow-xl">
+        <div className=" bg-blue-900 p-5 shadow-xl">
             <div className='text-white text-2xl'>
             <Image
-                src="/download.png"
+                src="/amazon.png"
                 layout='fixed'
-                height={120}
-                width={220}
+                height={100}
+                width={200}
             /> 
               Best-Sellers! Because you deserve only BEST ! </div>
             <input className="rounded-full  p-5 w-full text-gray-700 leading-tight focus:outline"
