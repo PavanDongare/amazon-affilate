3f299ba79f132abd71b6673c86b1a37aa56aa3cd
PavanDongare <dongare.pavan25@gmail.com>
2021-07-06 18:06:30 +0530

    wip

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,13 +5,13 @@ import Image from 'next/image'
 function Header(props) {
     return (
         <div className="flex flex-col sm:flex-row   bg-blue-900 p-2 ">
-                <Image className='rounded-lg'
+                <Image className='rounded-lg '
                     src="/amazon.png"
                     height={100}
                     width={200}
                 />
-                <div className='text-yellow-600 font-extrabold	 text-3xl'>            
-                    Best-Sellers!
+                <div className='text-yellow-600 font-extrabold	px-10 text-3xl'>            
+                    Best-Sellers
                 <div className='text-yellow-600 font-extrabold	 text-2xl'>You deserve the BEST !</div>
                  </div>
 
