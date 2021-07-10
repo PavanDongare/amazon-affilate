6699547cc3079e7a124802d8ca6c44c2056b65c7
PavanDongare <dongare.pavan25@gmail.com>
2021-07-11 02:25:57 +0530

    wip

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -6,11 +6,13 @@ import Search from '../Search'
 function Header(props) {
     return (
         <div className="flex flex-col sm:flex-row   bg-blue-900 p-2 ">
-                <Image className='rounded-lg '
+                <Image className='rounded-lg'
                     src="/amazon.png"
-                    height={100}
-                    width={200}
+                 
+                    height={10}
+                    width={120}
                 />
+              
                 <div className='text-white font-extrabold	px-10 text-3xl'>            
                     Best Sellers
                     <div className='text-yellow-600 font-extrabold	 text-2xl'>You deserve the BEST !</div>
@@ -18,6 +20,12 @@ function Header(props) {
                 <div className='flex-grow'>
                     <Search></Search>
                 </div>
+                <Image className='rounded-lg'
+                    src="/usa.jpg"
+        
+                    height={10}
+                    width={120}
+                />
                
         </div>
 
