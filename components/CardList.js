ee1e34e15d3cc1d0e8ed1b2fcc3e72a46415d4b2
PavanDongare <dongare.pavan25@gmail.com>
2021-07-11 02:06:04 +0530

    wip

--- components/CardList.js
+++ components/CardList.js
@@ -6,30 +6,50 @@ import { Fragment } from 'react';
 
 function CardList() {
     const { data, isLoading } = useProducts();
-    
+
     return (
         <Fragment>
-             {
-                data &&  Object.keys(data).map((e,i)=>{ return(
-                    <div>
-                        {e}
-                        <br></br>
-                        {JSON.stringify(data[e])}
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                    </div>)
-            
+            {
+                data && Object.keys(data).map((e, i) => {
+                    return (
+                        <div>
+                           
+                            <Card>  <div className='bg-blue-900 rounded-md text-white text-2xl w-full h-10 '> {e} </div> </Card>
+                           
+                            <Card>
+                            <div className='flex flex-wrap'>
+                            {data[e].map((x) => {
+                                return (<div>
+                                    <div className='flex flex-col w-40 p-2'
+                                        key={x.name}
+                                    >
+                                        <div>
+                                            <Image
+                                                src={x.thumbnail}
+                                                height={150}
+                                                width={150}
+                                            ></Image>
+                                        </div>
+                                        <div>  {x.name} </div>
+                                        <div> {x.category} </div>
+                                    </div>
+                                </div>)
+                            })}
+                            </div>
+                            
+                            </Card>
+                           
+                        </div>)
+
                 })
-             }
+            }
         </Fragment>
     )
 }
- 
-         
-     
-    
+
+
+
+
 
 
 export default CardList
